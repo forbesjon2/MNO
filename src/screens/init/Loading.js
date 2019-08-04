5e8e597656dcfd29103073a0cc603b088740dd08
forbesjon2 <forbesjon2@gmail.com>
2019-08-03 19:09:03 -0500

    working after issues upgrading

--- src/screens/init/Loading.js
+++ src/screens/init/Loading.js
@@ -1,10 +1,7 @@
 import React from 'react';
-import { View, InteractionManager, Text , Button} from "react-native";
-import { connect } from "react-redux";
-import MiniCalendar from "../../components/other/MiniCalendar";
-import srcStore from "../../Store";
-const {test} = require("../../Networking");
-import AsyncStorage from '@react-native-community/async-storage';
+import { View, Text , Button} from "react-native";
+import Store from "../../Store";
+
 
 export default class Loading extends React.Component{
     constructor(props){
@@ -16,14 +13,14 @@ export default class Loading extends React.Component{
         test("AB");
     }
     componentDidMount(){
-        srcStore.dispatch({type:"INIT"});
+        // srcStore.dispatch({type:"INIT"});
     }
     render(){
     
     return(
     <View style={{flex:1, backgroundColor:"black", marginTop:30}}>
         <Text>Loading screen</Text>
-        <Button onPress={() => this.props.dispatch({type:"SET_LOADED"})} title="set loaded"/>
+        <Button onPress={() => Store.dispatch({type:"SET_LOADED"})} title="set loaded"/>
         {/* <MiniCalendar /> */}
     </View>
     );        
